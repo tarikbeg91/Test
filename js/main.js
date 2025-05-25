14b73efd1694b6eb778fa3347ccedf18e2a69806
tarikbeg91 <tarikbeg91@gmail.com>
2025-05-25 16:57:47 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,46 @@
+// js/main.js
 
+// config.js से इम्पोर्ट्स
+import {
+    STORAGE_KEY_PIECES,
+    STORAGE_KEY_SETTINGS,
+    STORAGE_KEY_ID_COUNTER,
+    STORAGE_KEY_HISTORY,
+    STORAGE_KEY_LANGUAGE,
+    STORAGE_KEY_CURRENT_UNIT,
+    STORAGE_KEY_CUTTING_LIST_CHECKS,
+    STORAGE_KEY_PROJECTS,
+    STORAGE_KEY_GRAIN_COLOR,
+    STORAGE_KEY_GRAIN_PATTERN,
+    STORAGE_KEY_FITTING_STRATEGY,
+    STORAGE_KEY_SAVED_OFFCUTS,
+    STORAGE_KEY_OFFCUT_SETTINGS,
+    STORAGE_KEY_PROJECT_LOCATION,
+    STORAGE_KEY_PROJECT_NOTES,
+    MAX_HISTORY_STATES,
+    BASE_CANVAS_SCALE
+} from './config.js';
+
+// lang.js से इम्पोर्ट्स
+import { langStrings } from './lang.js';
+
+// utils.js से इम्पोर्ट्स
+import {
+    convertToCm,
+    convertFromCm,
+    convertFromCmForArea,
+    hslToRgb,
+    getRandomColor,
+    escapeCsvCell,
+    exportToCsv,
+    conversionFactorsToCm // यह utils.js से आ रहा है
+} from './utils.js';
+
+// --- यहाँ से एप्लिकेशन का बाकी लॉजिक शुरू होगा ---
+// (अगले चरणों में हम 214.html से कोड यहाँ स्थानांतरित करेंगे)
+
+// अभी के लिए, यह सुनिश्चित करने के लिए कि इम्पोर्ट काम कर रहे हैं, एक सरल console.log डालें:
+console.log("main.js loaded and modules imported.");
+console.log("A storage key from config:", STORAGE_KEY_PIECES);
+console.log("A lang string (hi):", langStrings.hi.appTitle);
+console.log("A util function result (10cm to mm):", convertFromCm(10, 'mm')); // यह काम करने के लिए conversionFactorsToCm को utils.js में होना चाहिए
